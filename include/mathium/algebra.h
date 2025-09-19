a07535cf5c30410ebbc8d14c3ccfd300ed26e2af
saintsHr <hiqkrs@gmail.com>
2025-09-19 18:57:11 -0500

    final testing

--- include/mathium/algebra.h
+++ include/mathium/algebra.h
@@ -1,7 +1,7 @@
 #pragma once
 
-inline long long clamp(long long x, long long min, long long max);
-inline long long map(long long x, long long in_min, long long in_max, long long out_min, long long out_max);
-inline long long lerp(long long a, long long b, double t);
-inline long long distance(long long x1, long long y1, long long x2, long long y2);
-inline long long wrap(long long x, long long min, long long max);
+long long clamp(long long x, long long min, long long max);
+long long map(long long x, long long in_min, long long in_max, long long out_min, long long out_max);
+long long lerp(long long a, long long b, double t);
+long long distance(long long x1, long long y1, long long x2, long long y2);
+long long wrap(long long x, long long min, long long max);
--- src/algebra.c
+++ src/algebra.c
@@ -1,31 +1,31 @@
 #include "mathium/algebra.h"
 #include "mathium/helpers.h"
 
-inline long long clamp(long long x, long long min, long long max){
+long long clamp(long long x, long long min, long long max){
     if (x < min) return min;
     if (x > max) return max;
     return x;
 }
 
-inline long long map(long long x, long long in_min, long long in_max, long long out_min, long long out_max) {
+long long map(long long x, long long in_min, long long in_max, long long out_min, long long out_max) {
     if (in_max == in_min) return out_min;
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-inline long long lerp(long long a, long long b, double t){
+long long lerp(long long a, long long b, double t){
     if (t >= 1.0) return b;
     if (t <= 0.0) return a;
 
     return a + (b - a) * t;
 }
 
-inline long long distance(long long x1, long long y1, long long x2, long long y2){
+long long distance(long long x1, long long y1, long long x2, long long y2){
     long long dx = x2 - x1;
     long long dy = y2 - y1;
     return sqrt_h(dx*dx + dy*dy);
 }
 
-inline long long wrap(long long x, long long min, long long max){
+long long wrap(long long x, long long min, long long max){
     if (max == min) return min;
     long long range = max - min;
     return ((x - min) % range + range) % range + min;
